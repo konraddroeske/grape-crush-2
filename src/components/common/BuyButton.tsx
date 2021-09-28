148d28c52bde99a38f53af8b7c8110e6092d8745
Konrad Droeske <kdroeske@gmail.com>
2021-09-28 11:36:51 -0400

    new buy button to cart

--- src/components/common/BuyButton.tsx
+++ src/components/common/BuyButton.tsx
@@ -1,5 +1,6 @@
-import React, { FunctionComponent } from 'react'
+import React, { FunctionComponent, useState } from 'react'
 
+import Minus from '../../assets/svgs/minus.svg'
 import Plus from '../../assets/svgs/plus.svg'
 
 interface OwnProps {
@@ -9,6 +10,8 @@ interface OwnProps {
 type Props = OwnProps
 
 const BuyButton: FunctionComponent<Props> = ({ productId }) => {
+  const [quantity, setQuantity] = useState<number>(1)
+
   const handleClick = () => {
     if (window?.AmbassadorChat) {
       window.AmbassadorChat.send({
@@ -18,34 +21,50 @@ const BuyButton: FunctionComponent<Props> = ({ productId }) => {
           {
             command: 'addToCart',
             productId,
+            quantity,
           },
         ],
       })
     }
   }
 
+  const handleQuantity = (amount: number) => {
+    const newAmount = quantity + amount
+    if (newAmount > 0) {
+      setQuantity(quantity + amount)
+    }
+  }
+
   return (
-    <div className="flex w-full max-w-2xs h-9 mt-auto border border-blue-dark">
-      <div
-        className="flex items-center pl-3 pr-2 mr-auto text-base
-      text-blue-dark font-bold uppercase line-clamp leading-9"
-      >
-        Add to Cart
+    <div className="flex justify-between w-full h-10 mt-auto max-w-lg">
+      <div className="flex border border-blue-dark mr-4 hover:bg-lime">
+        <button
+          type="button"
+          className="flex justify-center items-center w-12 h-full"
+          onClick={() => handleQuantity(-1)}
+        >
+          <Minus className="w-3" />
+        </button>
+        <div className="flex justify-center items-center w-10 text-blue-dark font-bold">
+          {quantity}
+        </div>
+        <button
+          type="button"
+          className="flex justify-center items-center w-12 h-full"
+          onClick={() => handleQuantity(1)}
+        >
+          <Plus className="w-3" />
+        </button>
       </div>
       <button
         type="button"
-        className="flex justify-center items-center w-10 border-l h-full
-        hover:bg-lime"
+        className="flex items-center flex-grow pl-3 pr-2 mr-auto border border-blue-dark text-base
+      text-blue-dark font-bold uppercase line-clamp leading-9 hover:bg-lime"
         onClick={() => handleClick()}
       >
-        <Plus className="w-3" />
+        <span className="xs:hidden">Add</span>
+        <span className="hidden xs:inline">Add to Cart</span>
       </button>
-      {/* <button */}
-      {/*  type="button" */}
-      {/*  className="h-full flex justify-center items-center w-10 hover:bg-lime" */}
-      {/* > */}
-      {/*  <Minus className="w-3" /> */}
-      {/* </button> */}
     </div>
   )
 }
