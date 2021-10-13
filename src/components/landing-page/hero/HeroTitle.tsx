517fb7b68375e0b87e25ac3a2c357500890bddbd
Konrad Droeske <kdroeske@gmail.com>
2021-10-13 17:06:01 -0400

    dummy invisible text when alternate title not visible

--- src/components/landing-page/hero/HeroTitle.tsx
+++ src/components/landing-page/hero/HeroTitle.tsx
@@ -1,18 +1,23 @@
 import React, { FunctionComponent } from 'react'
 
+import { useInView } from 'react-intersection-observer'
+
 import AnimatedHeadline from '@components/common/AnimatedHeadline'
 import AlternatingTitle from '@components/landing-page/hero/AlternatingTitle'
 import LowerTitle from '@components/landing-page/hero/LowerTitle'
 
 const HeroTitle: FunctionComponent = () => {
-  // const { ref, inView } = useInView({
-  //   threshold: 0,
-  // })
+  const { ref, inView } = useInView({
+    threshold: 0,
+  })
   return (
-    // <div ref={ref}>
-    <div>
+    <div ref={ref}>
       <h1 className="title-size uppercase font-bold flex flex-col text-center lg:text-left">
-        <AlternatingTitle />
+        {inView ? (
+          <AlternatingTitle />
+        ) : (
+          <span className="opacity-0">Natural</span>
+        )}
         <div className="flex flex-col lg:flex-row">
           <AnimatedHeadline text="Wines" textStyle="text-white" />
           <span className="hidden lg:block">&nbsp;</span>
