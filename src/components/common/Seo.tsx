d29b1cfdcb3d8f12de1f1407d88ad2f920406f6f
Konrad Droeske <kdroeske@gmail.com>
2021-10-20 13:55:44 -0400

    added fb app id to seo

--- src/components/common/Seo.tsx
+++ src/components/common/Seo.tsx
@@ -32,6 +32,9 @@ const Seo: FunctionComponent<Props> = ({ title, description, image }) => {
         "With one of Ontario's largest natural wines selection, shop from 200+ curated natural, biodynamic, and classic wines for any budget. We ship province-wide."
       }
       canonical="https://www.grapecrush.wine/"
+      facebook={{
+        appId: '1761199030730074',
+      }}
       openGraph={
         imageData
           ? {
