dc3f568ca26c75a0e2e95e18ed1d542456c5864d
Konrad Droeske <kdroeske@gmail.com>
2021-10-20 13:46:30 -0400

    added sitemap

--- next-sitemap.js
+++ next-sitemap.js
@@ -1,5 +1,5 @@
 module.exports = {
-  siteUrl: process.env.SITE_URL || 'https://grapecrush.wine',
-  generateRobotsTxt: true, // (optional)
-  // ...other options
+  siteUrl: 'https://www.grapecrush.wine',
+  generateRobotsTxt: true,
+  sitemapSize: 7000,
 }
