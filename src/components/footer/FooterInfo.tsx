5af01ca75b4f5cf1d2407365496333747d020f05
Konrad Droeske <kdroeske@gmail.com>
2021-10-20 08:30:07 -0400

    updated footer spacing

--- src/components/footer/FooterInfo.tsx
+++ src/components/footer/FooterInfo.tsx
@@ -12,7 +12,7 @@ import { selectGlobal } from '@redux/globalSlice'
 export const FooterTitle: FunctionComponent = ({ children }) => (
   <h4
     className="text-base font-bold center text-white text-center uppercase leading-tight
-            md:text-left xl:leading-7 lg:text-lg"
+            lg:text-left xl:leading-7 md:text-lg"
   >
     {children}
   </h4>
@@ -26,7 +26,7 @@ export const FooterLink: FunctionComponent<FooterLinkProps> = ({
   children,
   to,
 }) => (
-  <div className="py-1 text-sm md:mr-4 lg:mr-6 lg:text-base xl:mr-0 xl:leading-7 hover:text-lime">
+  <div className="py-1 text-sm lg:mr-6 md:text-base xl:mr-0 xl:leading-7 hover:text-lime">
     <Link href={to}>
       <a>{children}</a>
     </Link>
@@ -34,7 +34,9 @@ export const FooterLink: FunctionComponent<FooterLinkProps> = ({
 )
 
 export const LogoContainer: FunctionComponent = ({ children }) => (
-  <div className="w-8 mr-2 md:flex md:items-center xl:mr-4">{children}</div>
+  <div className="w-8 mx-3 lg:mx-0 lg:ml-6 lg:flex lg:items-center xl:ml-0 xl:mr-6">
+    {children}
+  </div>
 )
 
 const FooterInfo: FunctionComponent = () => {
@@ -47,13 +49,13 @@ const FooterInfo: FunctionComponent = () => {
         lg:body-gutter-lg xl:w-6/12 xl:items-center xl:pl-1/8 xl:pr-0 xl:pt-16"
       >
         <div
-          className="w-1/2 pr-1 md:w-auto md:flex-grow z-10 xl:w-1/2
+          className="w-1/2 pr-1 lg:w-auto lg:flex-grow z-10 xl:w-1/2
         xl:flex-grow-0 xl:pr-0"
         >
           <FooterTitle>Help</FooterTitle>
           <ul
             className="font-normal text-xs sm:text-sm text-white text-center mt-1 leading-5
-          md:flex xl:block xl:text-left"
+          lg:flex xl:block xl:text-left"
           >
             <li>
               <FooterLink to="/contact#contact-email">Contact</FooterLink>
@@ -66,25 +68,16 @@ const FooterInfo: FunctionComponent = () => {
             <li>
               <FooterLink to="/faq">Faq</FooterLink>
             </li>
-            {/* {helpPages.map((page) => { */}
-            {/*  return ( */}
-            {/*    <li key={page.slug[locale]}> */}
-            {/*      <FooterLink to={`/${page.slug[locale]}`}> */}
-            {/*        {page.title[locale]} */}
-            {/*      </FooterLink> */}
-            {/*    </li> */}
-            {/*  ) */}
-            {/* })} */}
           </ul>
         </div>
         <div
-          className="w-1/2 pl-1 md:w-auto md:flex-grow z-10 xl:w-1/2
+          className="w-1/2 pl-1 lg:w-auto lg:flex-grow z-10 xl:w-1/2
         xl:flex-grow-0 xl:pl-0"
         >
           <FooterTitle>Legal Info</FooterTitle>
           <ul
             className="font-normal text-xs sm:text-sm text-white text-center mt-1 leading-5
-          md:flex xl:block xl:text-left"
+          lg:flex xl:block xl:text-left"
           >
             {legalPages.map((page) => {
               return (
@@ -98,8 +91,8 @@ const FooterInfo: FunctionComponent = () => {
           </ul>
         </div>
         <div
-          className="flex flex-grow justify-center py-8 md:py-0 md:justify-end
-          md:flex-grow-0 sm:pb-0 xl:py-0 xl:ml-1/2"
+          className="flex flex-grow justify-center py-8 lg:py-0 lg:justify-end
+          lg:flex-grow-0 sm:pb-0 xl:py-0 xl:ml-1/2"
         >
           <LogoContainer>
             <a
