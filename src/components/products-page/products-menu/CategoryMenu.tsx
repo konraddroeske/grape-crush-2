af3aab7446632b3f08b8e72f3d472cc6ba8d8ac2
Konrad Droeske <kdroeske@gmail.com>
2021-10-22 09:47:46 -0400

    removed opacity on footer

--- src/components/products-page/products-menu/CategoryMenu.tsx
+++ src/components/products-page/products-menu/CategoryMenu.tsx
@@ -23,14 +23,14 @@ const CategoryMenu: FunctionComponent<Props> = ({
   const isSmall = useMediaQuery({ query: '(min-width: 640px)' })
   const isDesktop = useMediaQuery({ query: '(min-width: 1024px)' })
 
-  const [scrollHeight, setScrollHeight] = useState<number | null>(null)
+  const [scrollHeight, setScrollHeight] = useState<string>('none')
   const menuRef = useRef<null | HTMLUListElement>(null)
 
   const handleScrollHeight = () => {
     const height = menuRef?.current?.scrollHeight || 0
 
     if (height > 0) {
-      setScrollHeight(height)
+      setScrollHeight(`${height}px`)
     }
   }
 
@@ -41,11 +41,11 @@ const CategoryMenu: FunctionComponent<Props> = ({
   return (
     <ul
       ref={menuRef}
-      className="grid grid-cols-1 gap-y-4 overflow-hidden transition-all duration-300
+      className="grid grid-cols-1 gap-y-4 overflow-hidden transition-all duration-700
       sm:grid-cols-2 sm:gap-x-4 lg:grid-cols-1 lg:gap-x-0"
       style={{
         // eslint-disable-next-line no-nested-ternary
-        maxHeight: menuOpen ? (scrollHeight ? `${scrollHeight}px` : 'none') : 0,
+        maxHeight: menuOpen ? scrollHeight : 0,
       }}
     >
       {tagsWithProducts.map(({ name }) => {
