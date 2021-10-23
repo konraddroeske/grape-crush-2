16640296d434b8a3ee48391fc612bb907122c18c
Konrad Droeske <kdroeske@gmail.com>
2021-10-22 21:01:26 -0400

    fixed with varietal text

--- src/components/item-page/Fact.tsx
+++ src/components/item-page/Fact.tsx
@@ -11,7 +11,9 @@ const Fact: FunctionComponent<Props> = ({ category, value }) => {
   return (
     <div className="my-2">
       <span className="font-bold text-blue-dark uppercase">{category}: </span>
-      <span className="text-sm text-blue-dark uppercase">{value}</span>
+      <span className="text-sm text-blue-dark uppercase break-words">
+        {value}
+      </span>
     </div>
   )
 }
