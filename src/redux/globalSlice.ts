d10da5708794390183e59fd222f39fe4f397ee08
Konrad Droeske <kdroeske@gmail.com>
2021-10-28 18:32:17 -0400

    updated canonical and default image

--- src/redux/globalSlice.ts
+++ src/redux/globalSlice.ts
@@ -2,6 +2,7 @@ import { createSlice } from '@reduxjs/toolkit'
 import { HYDRATE } from 'next-redux-wrapper'
 
 import {
+  Asset,
   CONTENTFUL_DEFAULT_LOCALE_CODE,
   ICategoryFields,
   IFooterFields,
@@ -33,7 +34,7 @@ interface Global {
   mobileNavOpen: boolean
   isSticky: boolean
   heroSlides: IHeroSlideFields[]
-  // seoImage: Asset | null
+  seoImage: Asset | null
   currentTheme: Theme
   topStyles: string[]
   categories: ICategoryFields[]
@@ -50,7 +51,7 @@ const initialState: Global = {
   footer: [],
   nav: [],
   heroSlides: [],
-  // seoImage: null,
+  seoImage: null,
   currentTheme: {
     nav: '#2C148E',
     title: '#d9ff6c',
@@ -94,10 +95,10 @@ export const globalSlice = createSlice({
     setHeroSlides(state, action) {
       const { items } = action.payload
 
-      // const seoSlide = items.find((item: IHeroSlideFields) => item.mainImage)
-      // const { image: seoImage } = seoSlide
+      const seoSlide = items.find((item: IHeroSlideFields) => item.mainImage)
+      const { image: seoImage } = seoSlide
 
-      return { ...state, heroSlides: items }
+      return { ...state, seoImage, heroSlides: items }
     },
     setMobileNavOpen(state, action) {
       return { ...state, mobileNavOpen: action.payload }
