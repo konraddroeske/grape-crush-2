9d646af000b09d68d9f4303ddd6a506e15990b6c
Konrad Droeske <kdroeske@gmail.com>
2021-11-12 23:21:55 -0500

    simplified search

--- src/redux/clientSlice.ts
+++ src/redux/clientSlice.ts
@@ -6,35 +6,28 @@ interface Client {
   navSearch: string
   search: string
   modalOpen: boolean
-  // isLoading: boolean
 }
 
 const initialState: Client = {
   navSearch: '',
   search: '',
   modalOpen: true,
-  // isLoading: false,
 }
 
 export const clientSlice = createSlice({
   name: 'client',
   initialState,
   reducers: {
-    setNavSearch(state, action) {
-      return { ...state, navSearch: action.payload }
-    },
     closeModal(state) {
       return { ...state, modalOpen: false }
     },
     setSearch(state, action) {
+      // console.log('setting search', action.payload)
       return { ...state, search: action.payload }
     },
-    // setIsLoading(state, action) {
-    //   return { ...state, isLoading: action.payload }
-    // },
   },
 })
 
-export const { setNavSearch, closeModal, setSearch } = clientSlice.actions
+export const { closeModal, setSearch } = clientSlice.actions
 
 export const selectClient = () => (state: AppState) => state?.[clientSlice.name]
